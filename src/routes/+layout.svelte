f3594371d1146fed2a202d952229a0d18d4d6ebe
Ben Kates <benekates@gmail.com>
2024-09-11 16:23:45 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 </script>
 
 <PymChild polling={250} />
+<svelte:head><title>Unstable Work is All Too Common, Especially for Black Women</title></svelte:head
+>
 
 <FontsUrban />
 <Analytics title="black-women-precarious-gig-work" mode={import.meta.env.MODE} />
